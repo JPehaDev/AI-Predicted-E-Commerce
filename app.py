8b1ff39d5da836ae10eb355abd40ec4b903db68f
cesjavi <cesjavi@gmail.com>
2024-01-18 16:45:45 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from pydantic import BaseModel
 from src.preprocessdata import read_data
 from src.settings import redis_port
 from src.predict import main_predict
 import redis
 from typing import Union
+import uuid
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
-app =  FastAPI(host="localhost", title="ecommerce", description="API for ecommerce ml model", version="1.0", docs_url="/")
+app =  FastAPI(host="localhost", title="ecommerce", description="API for ecommerce ml model", version="1.0", docs_url="/docs")
 redis_client = redis.StrictRedis(host='redis', port=redis_port, db=0)
+templates = Jinja2Templates(directory="templates")
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class Item(BaseModel):
     sku: str
@@ -58,4 +66,37 @@ async def update_predict_category(sku: str) -> dict:
         "name": item[b"name"], "description": item[b"description"],
             "category": item[b"category"], "predict_category": predict_category_value
             })
+    return redis_client.hgetall(str(sku))
+
+@app.get("/", response_class=HTMLResponse)
+async def read_form(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
+
+@app.post("/new_item_and_predict/")
+async def new_item_and_predict(request: Request, name:str = Form(...), description:str = Form(...), category:str = Form(None)) -> dict:
+    import random
+    import redis
+    
+    # Generate a random number between 1,000,000 and 9,999,999
+    sku = random.randint(1000000, 9999999)
+
+# Check if the generated number already exists in Redis
+    while redis_client.exists(sku):
+    # If it exists, generate a new number
+        sku = random.randint(1000000, 9999999)
+
+
+    predict_category_value = main_predict(name, description)
+    item = Item(sku=str(sku), name=str(name), description=str(description), category=str(category), predict_category=str(predict_category_value))
+
+    redis_client.hmset(str(item.sku), {
+        "name": str(item.name), "description": str(item.description),
+        "category": str(item.category), "predict_category": str(item.predict_category)
+        })
+
+    return templates.TemplateResponse("result.html", {"request": request, "item": item})
+
+
+@app.get("/get_item/")
+async def read_items(sku: str) -> dict:
     return redis_client.hgetall(str(sku))
